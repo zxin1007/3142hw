3136b99a800888a33b7624b47c070ad22426615c
Zixin Zou <zxinmm@gmail.com>
2022-04-27 09:16:45 -0400

    different function for getting pass_rate, w_rate, etc

---
+++ lab3/src/function.h
@@ -0,0 +1,136 @@
+#include <vector>
+#include <type_traits>
+#include <map>
+#include <string> 
+using namespace std;
+
+int count (vector<Student> vector,string instructor_id ,bool pass,int mode);
+map<string,int> max (vector<Student> vector,int mode);
+
+void pass_rate(vector<Student> vector, int mode){
+
+    map<string,int> myMap = max(vector,mode);
+
+    if (mode == 1){
+        ofstream myfile;
+        myfile.open ("../output/passratePerInstructor.txt");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int pass_grade = count(vector,it->first,true,mode);
+            myfile << "passing rate for instructor " << it->first << " is " << (float)pass_grade/it->second << endl;
+        }
+        myfile.close();
+    } else if (mode == 2){
+        ofstream myfile;
+        myfile.open ("../output/passratePerCourseNumber.txt");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int pass_grade = count(vector,it->first,true,mode);
+            myfile << "passing rate for course number " << it->first << " is " << (float)pass_grade/it->second << endl;
+        }
+        myfile.close();
+    } else if (mode == 3){
+        ofstream myfile;
+        myfile.open ("../output/passratePerTerm.txt");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int pass_grade = count(vector,it->first,true,mode);
+            myfile << "passing rate for term " << it->first << " is " << (float)pass_grade/it->second << endl;
+        }
+        myfile.close();
+    }
+}
+
+void w_rate(vector<Student> vector, int mode){
+
+    map<string,int> myMap = max(vector,mode);
+
+    if (mode == 1){
+        ofstream myfile;
+        myfile.open ("../output/wratePerInstructor.txt");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int w_grade = count(vector,it->first,false,mode);
+            myfile << "w rate for instructor " << it->first << " is " << (float)w_grade/it->second << endl;
+        }
+        myfile.close();
+    } else if (mode == 2){
+        ofstream myfile;
+        myfile.open ("../output/wratePerCourseNumber");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int w_grade = count(vector,it->first,false,mode);
+            myfile << "w rate for course number " << it->first << " is " << (float)w_grade/it->second << endl;
+        }
+        myfile.close();
+    } else if (mode == 3){
+        ofstream myfile;
+        myfile.open ("../output/wratePerTerm.txt");
+        for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+            int w_grade = count(vector,it->first,false,mode);
+            myfile << "w rate for term " << it->first << " is " << (float)w_grade/it->second << endl;
+        }
+        myfile.close();
+    }
+
+}
+
+//count the pass/fail rate for each instructor
+int count (vector<Student> vector,string condition ,bool pass,int mode){
+    int amount = 0;
+    for (int i = 0; i < vector.size(); i++)
+    {
+        if (mode == 1){
+            if (pass && vector[i].instructor.instructor_id==condition&&vector[i].pass=='p'){
+                amount+=1;
+            } else if (!pass && vector[i].instructor.instructor_id==condition && vector[i].pass=='w'){
+                amount+=1;
+            }
+        } else if (mode == 2){
+            if (pass && to_string(vector[i].instructor.course.course_id)==condition&&vector[i].pass=='p'){
+                amount+=1;
+            } else if (!pass && to_string(vector[i].instructor.course.course_id)==condition && vector[i].pass=='w'){
+                amount+=1;
+            }
+        } else if (mode == 3){
+            if (pass && vector[i].instructor.term.term_name==condition&&vector[i].pass=='p'){
+                amount+=1;
+            } else if (!pass && vector[i].instructor.term.term_name==condition && vector[i].pass=='w'){
+                amount+=1;
+            }
+        }
+    } 
+
+    return amount;
+    
+}
+
+map<string,int> max (vector<Student> vector, int mode){
+    map<string,int> myMap;
+    for (int i = 0; i < vector.size(); i++)
+    {
+        map<string,int>::iterator it = myMap.begin();
+        if (mode == 1){
+            for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+                if (vector[i].instructor.instructor_id.c_str() == it->first){
+                    int num = myMap.at(vector[i].instructor.instructor_id);
+                    myMap.at(vector[i].instructor.instructor_id) = num+1;
+                }
+            }
+            myMap.insert(pair<string,int>(vector[i].instructor.instructor_id,1));
+        } else if (mode == 2){
+            for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+                if (to_string(vector[i].instructor.course.course_id) == it->first){
+                    int num = myMap.at(to_string(vector[i].instructor.course.course_id));
+                    myMap.at(to_string(vector[i].instructor.course.course_id)) = num+1;
+                }
+            }
+            myMap.insert(pair<string,int>(to_string(vector[i].instructor.course.course_id),1));
+        } else if (mode ==3){
+            for (map<string,int>::iterator it=myMap.begin(); it!=myMap.end(); ++it){
+                if (vector[i].instructor.term.term_name == it->first){
+                    int num = myMap.at(vector[i].instructor.term.term_name);
+                    myMap.at(vector[i].instructor.term.term_name) = num+1;
+                }
+            }
+            myMap.insert(pair<string,int>(vector[i].instructor.term.term_name,1));
+        }
+    }
+
+    return myMap;
+}
