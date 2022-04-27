15fa5a62fb0dcfeaaac62ed1595ea736b4a0c9bc
Zixin Zou <zxinmm@gmail.com>
2022-04-27 09:15:50 -0400

    include instructor in student struct

--- lab3/src/record.h
+++ lab3/src/record.h
@@ -5,11 +5,6 @@ struct Term
 {
     string term_id;
     string term_name;
-
-    bool operator!=(const Term& other) const
-    {
-        return term_id != other.term_id;
-    }
 };
 
 struct Course
@@ -19,16 +14,17 @@ struct Course
     struct Term term;
 };
 
-struct Student
-{
-    string id;
-    string grade;
-    Course course;
-};
-
 struct Instructor
 {
     string instructor_id;
     struct Course course;
     Term term;
+};
+
+struct Student
+{
+    string id;
+    string grade;
+    char pass;
+    struct Instructor instructor;
 };
